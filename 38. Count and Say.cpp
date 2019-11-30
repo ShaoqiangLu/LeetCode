0b356195153e1b1c7be4b9d2efede393e3b84261
ShaoqiangLu <lushaoqiang27@gmail.com>
2019-11-30 15:27:23 +0800

    add

--- 38. Count and Say.cpp
+++ 38. Count and Say.cpp
@@ -5,24 +5,20 @@ using namespace std;
 class Solution {
 public:
   string countAndSay(int n) {
-    string s = "1";
-    for (int i = 1; i < n; i++) {
+    string res = "1";
+    while (--n) {
       string tmp;
-      char flag = s[0];
-      int count = 0;
-      for (auto c : s) {
-        if (c == flag)
+      for (size_t i = 0; i < res.size(); i++) {
+        int count = 1;
+        while (i + 1 < res.size() && res[i] == res[i + 1]) {
           count++;
-        else {
-          tmp.append(to_string(count) + flag);
-          flag = c;
-          count = 1;
+          i++;
         }
+        tmp.append(to_string(count) + res[i]);
       }
-      tmp.append(to_string(count) + flag);
-      s = tmp;
+      res = tmp;
     }
-    return s;
+    return res;
   }
 };
 
