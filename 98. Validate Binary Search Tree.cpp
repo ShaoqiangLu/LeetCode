2c6895aa5e2cb0aa9d223e13c0411d98e95156b1
ShaoqiangLu <lushaoqiang27@gmail.com>
2019-12-27 23:37:35 +0800

    add

--- 98. Validate Binary Search Tree.cpp
+++ 98. Validate Binary Search Tree.cpp
@@ -1,18 +1,29 @@
 #include "TreeNode.h"
+#include <climits>
 
 class Solution {
 public:
   bool isValidBST(TreeNode *root) {
+    return isValidBST(root->left, INT_MIN, root->val) &&
+           isValidBST(root->right, root->val, INT_MAX);
+  }
+
+  bool isValidBST(TreeNode *root, int low, int high) {
     if (!root)
       return true;
-    if (!isValidBST(root->left))
+    if (root->val <= low)
       return false;
-      if()
+    if (root->val >= high)
+      return false;
+    return isValidBST(root->left, low, root->val) &&
+           isValidBST(root->right, root->val, high);
   }
 };
 
 int main() {
   TreeNode *t1 = new TreeNode(1);
-  bool r = Solution().isValidBST(t1, t2);
+  t1->left = new TreeNode(0);
+  t1->right = new TreeNode(3);
+  bool r = Solution().isValidBST(t1);
   return 0;
 }
