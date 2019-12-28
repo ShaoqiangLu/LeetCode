e80f9297847c09c60af7085f22cd9823d7fc6929
ShaoqiangLu <lushaoqiang27@gmail.com>
2019-12-28 15:51:10 +0800

    add

--- 31. Next Permutation.cpp
+++ 31. Next Permutation.cpp
@@ -1,14 +1,32 @@
+#include <algorithm>
 #include <vector>
 
 using namespace std;
 
 class Solution {
 public:
-  void nextPermutation(vector<int> &nums) {}
+  void nextPermutation(vector<int> &nums) {
+    if (nums.size() <= 1)
+      return;
+    int l = -1, r = nums.size() - 1;
+    for (int i = nums.size() - 2; i >= 0; i--)
+      if (nums[i] < nums[i + 1]) {
+        l = i;
+        r = l + 1;
+        break;
+      }
+    for (int i = r + 1; i < nums.size(); i++)
+      if (nums[i] > nums[l] && nums[i] <= nums[r])
+        r = i;
+    if (l >= 0)
+      swap(nums[l], nums[r]);
+    reverse(nums.begin() + l + 1, nums.end());
+    return;
+  }
 };
 
 int main() {
-  vector<int> v{4, 5, 6, 7, 0, 1, 2};
+  vector<int> v{1, 1};
   Solution().nextPermutation(v);
   return 0;
 }
---
+++ 47. Permutations II.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  vector<vector<int>> permuteUnique(vector<int> &nums) {
+    vector<int> tmp;
+    vector<vector<int>> res;
+    permute(nums, 0, res);
+    return res;
+  }
+
+  void permute(vector<int> &nums, int idx, vector<vector<int>> &res) {
+    int n = nums.size();
+    if (idx == nums.size())
+      res.push_back(nums);
+    for (int i = idx; i < n; i++) {
+      swap(nums[idx], nums[i]);
+      permute(nums, idx + 1, res);
+      swap(nums[idx], nums[i]);
+    }
+  }
+};
+
+int main() {
+  vector<int> v{1, 2, 3};
+  auto r = Solution().permuteUnique(v);
+  return 0;
+}
